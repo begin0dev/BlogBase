ed3b1bba78615de8c25613f932489e3ba190401a
prosonic1 <prosonic1@naver.com>
2018-08-28 19:46:25 +0900

    react typescript

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -149,7 +149,7 @@ module.exports = {
             test: /\.(js|jsx)$/,
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
-            options: {              
+            options: {
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { PageTemplate } from 'components';
-import { MainPage, ProfilePage, CategoryPage, NotFoundPage } from 'pages';
+import { MainPage, ProfilePage, CategoryPage, NotFoundPage } from 'pages/index';
 
 const App = () => (
   <PageTemplate>
