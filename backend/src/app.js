450c105af59ed73a7cd43c598827c540a23c3cb8
beginner <prosonic1@gmail.com>
2018-12-01 21:07:47 +0900

    Update oauth

--- backend/src/app.js
+++ backend/src/app.js
@@ -53,6 +53,7 @@ app.use((req, res, next) => {
 
 /* handle error */
 app.use((err, req, res, next) => {
+  console.error(err);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
