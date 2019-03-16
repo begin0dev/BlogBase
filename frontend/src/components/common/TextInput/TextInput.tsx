4d1acfa0c4d0c02f3a24ea32f766b9bbb0adcc1c
begin0dev <prosonic1@gmail.com>
2019-03-16 19:43:36 +0900

    apply react hooks

--- frontend/src/components/common/TextInput/TextInput.tsx
+++ frontend/src/components/common/TextInput/TextInput.tsx
@@ -5,9 +5,6 @@ import styles from './TextInput.module.scss';
 
 const cx = classNames.bind(styles);
 
-interface IState {
-  focus: boolean;
-}
 interface IProps {
   type: string;
   name: string;
@@ -18,32 +15,33 @@ interface IProps {
   setValue(e: React.ChangeEvent<HTMLInputElement>): void;
 }
 
-class TextInput extends React.Component<IProps, IState> {
-  inputRef!: HTMLInputElement;
+const TextInput: React.FunctionComponent<IProps> = ({
+  type,
+  name,
+  label,
+  placeholder,
+  value,
+  color,
+  setValue,
+}) => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [focus, setFocus] = React.useState<boolean>(false);
 
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      focus: false,
-    };
-  }
+  const labelClick = (): void => {
+    if (!inputRef || !inputRef.current) return;
+    if (!focus) inputRef.current.focus();
+  };
 
-  labelClick = (): void => {
-    const { inputRef } = this;
-    const { focus } = this.state;
-    if (!focus) inputRef.focus();
+  const toggleInputFocus = (focusing: boolean) => (): void => {
+    setFocus(focusing);
   };
-  toggleInputFocus = (focusing: boolean) => (): void => {
-    this.setState({
-      focus: focusing,
-    });
-  };
-  labelPosition = (): object => {
-    const { inputRef } = this;
-    const { focus } = this.state;
-    const { placeholder } = this.props;
+  const labelPosition = (): object => {
     let position = {};
-    if (focus || placeholder || (inputRef && inputRef.value.length > 0)) {
+    if (
+      focus ||
+      placeholder ||
+      (inputRef && inputRef.current && inputRef.current.value.length > 0)
+    ) {
       position = {
         transform: 'translateY(-1.2rem) scale(0.88)',
         fontWeight: '400',
@@ -52,49 +50,36 @@ class TextInput extends React.Component<IProps, IState> {
     return position;
   };
 
-  render() {
-    const { focus } = this.state;
-    const { toggleInputFocus, labelPosition, labelClick } = this;
-    const {
-      type,
-      name,
-      label,
-      placeholder,
-      value,
-      color,
-      setValue,
-    } = this.props;
-    return (
-      <div className={cx('wrapper')}>
-        <div
-          className={cx('input-slot', { 'input-focus': focus })}
-          style={{ color }}
-        >
-          {label && (
-            <label
-              className={cx('label')}
-              aria-hidden="true"
-              style={{ ...labelPosition() }}
-              onClick={labelClick}
-            >
-              {label}
-            </label>
-          )}
-          <input
-            className={cx('input')}
-            type={type}
-            name={name}
-            value={value}
-            placeholder={placeholder}
-            onFocus={toggleInputFocus(true)}
-            onBlur={toggleInputFocus(false)}
-            onChange={setValue}
-            ref={(input: HTMLInputElement) => (this.inputRef = input)}
-          />
-        </div>
+  return (
+    <div className={cx('wrapper')}>
+      <div
+        className={cx('input-slot', { 'input-focus': focus })}
+        style={{ color }}
+      >
+        {label && (
+          <label
+            className={cx('label')}
+            aria-hidden="true"
+            style={{ ...labelPosition() }}
+            onClick={labelClick}
+          >
+            {label}
+          </label>
+        )}
+        <input
+          className={cx('input')}
+          type={type}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          onFocus={toggleInputFocus(true)}
+          onBlur={toggleInputFocus(false)}
+          onChange={setValue}
+          ref={inputRef}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default TextInput;
